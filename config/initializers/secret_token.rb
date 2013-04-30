b26ef1b1276b6f454e62a1ac3715442c61550cb5
unknown <nyak.nya@mail.ru>
2013-04-30 21:03:55 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Danill::Application.config.secret_token = '0105f1aaf0b6d56e139eee64d9b3ee4bd939fe6f2dc19f7e9f6034b859bc5b26b4957365a2f87c8fc33dd35c5981581f222d00bce49c643769deb7985dfac962'
